283ffc36edf3cc0b96f5c61a3fc7dda651f88540
Andenet Alexander <59474650+andyalex234@users.noreply.github.com>
2022-08-12 21:11:54 +0300

    cleanup clean_tweets_dataframe.py

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 class Clean_Tweets:
     """
@@ -30,7 +31,7 @@ class Clean_Tweets:
         """
         convert column to datetime
         """
-        df = df.apply(pd.to_datetime, args=['created_at'])
+        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
         
         return df
     
@@ -53,3 +54,66 @@ class Clean_Tweets:
         df = df[df['lang'] == 'en']
 
         return df
+    
+    def drop_nulls(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        drop rows that have null values
+        """
+        df = df.dropna(axis=0, how='any', inplace=False)
+
+        return df
+       
+    def text_category(self, series:pd.Series)->pd.Series:
+        """
+        find the category of the tweet
+        """
+        polarities = []
+        for polarity in series:
+            if polarity < 0:
+                polarities.append('negative')
+            elif polarity > 0:
+                polarities.append('positive')
+            else:
+                polarities.append('neutral')
+        return polarities
+    
+    def fill_missing(self, df: pd.DataFrame, column: str, value: str = 'unknown'):
+        """
+        fill null values of a specific column with the provided value
+        """
+
+        df[column] = df[column].fillna(value)
+
+        return df
+
+    def replace_empty_string(self, df:pd.DataFrame, column: str, value: str):
+        """
+        replace empty strings in a specific column with the provided value
+        """
+
+        df[column] = df[column].apply(lambda x: value if x == "" else x)
+
+        return df
+    def remove_characters(self, df: pd.DataFrame, column: str):
+        """
+        removes non-alphanumeric characters with the exception of underscore hyphen and space
+        from the specified column
+        """
+
+        df[column] = df[column].apply(lambda text: re.sub("[^a-zA-Z0-9\s_-]", "", text))
+
+        return df
+    def extract_device_name(self, df: pd.DataFrame) -> pd.Series:
+        """
+        returns device name from source text
+        """
+        df["source"] = df["source"].str.replace(r"(\s*\<.*?\>\s*)", " ", regex=True).str.strip()
+             
+
+        return df["source"]
+
+if __name__ == "__main__":
+    df = pd.read_csv("./processed_tweet_data.csv")
+    clean_tweets = Clean_Tweets(df)
+    df.to_csv('clean_tweets.csv', index=False)
+    print('Automation Completed...!!!')
