1dc7ff384b4998f75bffd68cacbdc85954d6855e
Andenet Alexander <andyalex234@gmail.com>
2022-12-28 16:34:18 +0300

    added build model module

---
+++ build_model.py
@@ -0,0 +1,27 @@
+from gensim import corpora, models
+from gensim.utils import SaveLoad
+from gensim.matutils import corpus2csc, sparse2full, corpus2dense
+
+
+class BuildModel():
+    def __init__(self) -> None:
+        pass
+
+    def makeDict(self, myTweetList):
+        """Create dictionary from list of tokenized documents"""
+        return corpora.Dictionary(myTweetList)
+
+    def makeCorpus(self, myTweetList, myDict):
+        """Create corpus from list of tokenized documents"""
+        return [myDict.doc2vow(tweet) for tweet in myTweetList]
+    
+    def createLDA(self, myCorpus, myDictionary, myTopics = 50, myPasses = 10, myIterations = 50, myAlpha=0.001):
+        """LDA model call function"""
+        return models.LdaMulticore(myCorpus, id2word=myDictionary, num_topics=myTopics, passes=myPasses, iterations=myIterations, alpha=myAlpha)
+    
+    def saveModelObjects(myLda: models.LdaMulticore, myModelName:str, myCorp, myCopName:str, myDict, myDictName:str):
+        """Save model objects"""
+        SaveLoad.save(myLda, myModelName)
+        corpora.MmCorpus.serialize(myCopName, myCorp)
+        myDict.save(myDictName)
+        print("Model Objects Successfully SAVED!")
